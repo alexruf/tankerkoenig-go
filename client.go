2faa763fe605d60441c4bedca54ea984bc555466
Jannick Fahlbusch <git@jf-projects.de>
2016-12-10 18:03:56 +0100

    Fix goVet errors
    This replaces the shadowing declaration of
    `err`.

--- client.go
+++ client.go
@@ -82,7 +82,7 @@ func (c *Client) NewRequest(method, urlStr string, query url.Values, body interf
 
 	buf := new(bytes.Buffer)
 	if body != nil {
-		err := json.NewEncoder(buf).Encode(body)
+		err = json.NewEncoder(buf).Encode(body)
 		if err != nil {
 			return nil, err
 		}
@@ -130,12 +130,12 @@ func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
 
 	if v != nil {
 		if w, ok := v.(io.Writer); ok {
-			_, err := io.Copy(w, resp.Body)
+			_, err = io.Copy(w, resp.Body)
 			if err != nil {
 				return nil, err
 			}
 		} else {
-			err := json.NewDecoder(response.Body).Decode(v)
+			err = json.NewDecoder(response.Body).Decode(v)
 			if err != nil {
 				return nil, err
 			}
